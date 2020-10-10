a12dc2b8a832efaf41b6d3da3738007040b5dc3d
Aimcom <robinhoodblock71@gmail.com>
2020-10-09 22:45:41 -0600

    Updates

---
+++ precache-manifest.5398dcfab0821316748a33a66798492d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae1f25d4e1c8fa2ba568",
+    "url": "/the-peoples-exchange-dex/static/js/runtime~main.ae1f25d4.js"
+  },
+  {
+    "revision": "ee54b3c9cac983ef374d",
+    "url": "/the-peoples-exchange-dex/static/js/main.ee54b3c9.chunk.js"
+  },
+  {
+    "revision": "cf90162d6bb05ae2cb8c",
+    "url": "/the-peoples-exchange-dex/static/js/1.cf90162d.chunk.js"
+  },
+  {
+    "revision": "ee54b3c9cac983ef374d",
+    "url": "/the-peoples-exchange-dex/static/css/main.57c145de.chunk.css"
+  },
+  {
+    "revision": "cf90162d6bb05ae2cb8c",
+    "url": "/the-peoples-exchange-dex/static/css/1.ed53cb5d.chunk.css"
+  },
+  {
+    "revision": "d1a11c8c0cf73f5431232a1e45efe2a0",
+    "url": "/the-peoples-exchange-dex/index.html"
+  }
+];
